2a81c96881c313ab859bff30b76e02efaad2003b
david <2657070472@qq.com>
2018-11-01 14:41:58 +0800

    类型转换

--- cs_lesson5.1/Program.cs
+++ cs_lesson5.1/Program.cs
@@ -6,10 +6,38 @@ using System.Threading.Tasks;
 
 namespace cs_lesson5._1
 {
-    class Program
+    class Program  //类型转换
     {
         static void Main(string[] args)
         {
+            short shortResult, shortVal = 4;
+            int integerVal = 67;
+            long longResult;
+            float floatVal = 10.5F;
+            double doubleResult, doubleVal = 99.999;
+            string stringResult, stringVal = "17";
+            bool boolVal = true;
+
+            Console.WriteLine("Variable Conversion Example\n");
+            doubleResult = floatVal * shortVal;   
+            Console.WriteLine("Implicit, -> double: {0}*{1} ->{2}",
+                floatVal,shortVal,doubleResult);
+
+            shortResult = (short)floatVal;
+            Console.WriteLine("Explicit, -> short:{0} -> {1}",
+                floatVal, shortResult);
+
+            stringResult = Convert.ToString(boolVal) + 
+                Convert.ToString(doubleVal);
+            Console.WriteLine("Explicit, -> string: \"{0}\" + \"{1}\" -> {2}",
+                boolVal, doubleVal, stringResult);
+
+            longResult = integerVal + Convert.ToInt64(stringVal);
+            Console.WriteLine("Mixed, -> long: {0} + {1} -> {2}",
+                integerVal, stringVal, longResult);
+            Console.ReadKey();        
+                         
+
         }
     }
 }
